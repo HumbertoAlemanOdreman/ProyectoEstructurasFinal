e8c0feb6c0ce69f4a2fe5c0f90e354e9b03dcb25
HumbertoAlemanOdreman <haleman.23@est.ucab.edu.ve>
2023-12-28 15:57:01 -0400

    Added functions to main
    Added "Bone" structures to the main.c file

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "Articles.c"
+#include "Vendors.c"
+#include "Clients.c"
+
+ArticleNode* ArticleList = NULL;
+VendorNode* VendorList = NULL;
+ClientNode* ClientList = NULL;
 
 int main(void) {
+    ReadFileArticle(&ArticleList, "Articulos.txt");
+    ReadFileVendor(&VendorList, "Vendedores.txt");
+    ReadFileClient(&ClientList, "Clientes.txt");
+    PrintClientList(ClientList);
     return 0;
 }
